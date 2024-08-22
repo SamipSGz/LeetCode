e4e421f07d9b3ce549b08aa014491946c61181d2
SamipSGz <samipgh444@gmail.com>
2024-08-22 09:22:39 +0545

    solved Q476

--- 664. Strange Printer.cpp
+++ 664. Strange Printer.cpp
@@ -1,21 +1,30 @@
-class Solution {
+#include <string>
+#include <vector>
+class Solution
+{
 public:
-    int strangePrinter(string s) {
+    int strangePrinter(string s)
+    {
         int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             dp[i][i] = 1;
         }
 
-        for (int len = 2; len <= n; ++len) {
-            for (int i = 0; i <= n - len; ++i) {
+        for (int len = 2; len <= n; ++len)
+        {
+            for (int i = 0; i <= n - len; ++i)
+            {
                 int j = i + len - 1;
-                dp[i][j] = len; 
+                dp[i][j] = len;
 
-                for (int k = i; k < j; ++k) {
+                for (int k = i; k < j; ++k)
+                {
                     int total = dp[i][k] + dp[k + 1][j];
-                    if (s[i] == s[j]) {
+                    if (s[i] == s[j])
+                    {
                         total--;
                     }
                     dp[i][j] = min(dp[i][j], total);
