f1d2a9539293c38dc49fc76b89dd0477847605d4
SamipSGz <samipgh444@gmail.com>
2024-09-03 08:09:24 +0545

    Solved Q1945

--- 1945. Sum of Digits of String After Convert.cpp
+++ 1945. Sum of Digits of String After Convert.cpp
@@ -1,24 +1,27 @@
 #include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int getLucky(string s, int k) {
-        // Step 1: Convert the string into a string of digits
+    int getLucky(string s, int k)
+    {
         string transformed;
-        for (char c : s) {
-            int pos = c - 'a' + 1; // Calculate position in the alphabet
+        for (char c : s)
+        {
+            int pos = c - 'a' + 1;
             transformed += to_string(pos); // Append position as string
         }
 
-        // Step 2: Sum the digits k times
         int sum = 0;
-        for (int i = 0; i < k; ++i) {
+        for (int i = 0; i < k; ++i)
+        {
             sum = 0;
-            for (char digit : transformed) {
+            for (char digit : transformed)
+            {
                 sum += digit - '0'; // Convert char to int and add to sum
             }
-            transformed = to_string(sum); // Convert sum back to string for next iteration
+            transformed = to_string(sum);
         }
 
         return sum;
