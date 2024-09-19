89f0d8b9918be326b5ee05409e0c3ae6f4a989d0
SamipSGz <samipgh444@gmail.com>
2024-09-19 09:11:34 +0545

    Solved Q241

---
+++ 241. Different Ways to Add Parentheses.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> diffWaysToCompute(string expression)
+    {
+        vector<int> output;
+        for (int i = 0; i < expression.length(); i++)
+        {
+            if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*')
+            {
+                vector<int> left = diffWaysToCompute(expression.substr(0, i));
+                vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+                for (auto x : left)
+                {
+                    for (auto y : right)
+                    {
+                        if (expression[i] == '+')
+                        {
+                            output.push_back(x + y);
+                        }
+                        if (expression[i] == '*')
+                        {
+                            output.push_back(x * y);
+                        }
+                        if (expression[i] == '-')
+                        {
+                            output.push_back(x - y);
+                        }
+                    }
+                }
+            }
+        }
+        if (output.empty())
+        {
+            output.push_back(stoi(expression));
+        }
+
+        return output;
+    }
+};
